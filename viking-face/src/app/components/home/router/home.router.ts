16727accaba6fad55350ec1b499aba7c1b349728
Víctor Vicente <victorvicente@MBP-de-Victor.home>
2018-10-06 22:20:03 +0200

    Added new routes

--- viking-face/src/app/components/home/router/home.router.ts
+++ viking-face/src/app/components/home/router/home.router.ts
@@ -5,9 +5,10 @@ import { HomePresenter } from '../presenter/home.presenter';
 export class HomeRouter {
     public static routes: Routes = [
         { path: 'home', component: HomePresenter },
+        { path: 'presentation', component: HomePresenter },
+        { path: 'collaborations', component: HomePresenter },
+        { path: 'skills', component: HomePresenter },
+        { path: 'carrer', component: HomePresenter },
         { path: '', pathMatch: 'full', redirectTo: 'first' }
-        // { path: ‘first’, component: FirstComponent },
-        // { path: ‘second’, component: SecondComponent },
-        // { path: ‘third’, component: ThirdComponent },
       ];
 }
